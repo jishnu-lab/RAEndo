284c4e29723cddd877c38a3da356e54c4a5953b0
jrahimik <jrahimik1986@gmail.com>
2024-09-09 14:00:17 -0400

    adding all of the files

---
+++ analysis/heritability/make_estimation.sh
@@ -0,0 +1,5 @@
+
+/ix/djishnu/Javad/racer/racer-genetics/racer-imputed/LDAK/ldak5.2.linux  --covar /ix/djishnu/Javad/racer/racer-genetics/racer-imputed/LDAK_reml_analysis/modules/bash/1PC-gender.cov  --reml k0.25   --grm racer-mega-imputed-f-matched-v3-QC  --pheno "/ix/djishnu/Javad/racer/racer-genetics/racer-imputed/racer-mega-imputed-f-matched-v3-QC.fam" --mpheno 4 
+
+
+
---
+++ analysis/heritability/make_grms.sh
@@ -0,0 +1,3 @@
+/ix/djishnu/Javad/racer/racer-genetics/racer-imputed/LDAK/ldak5.2.linux --ignore-weights YES --bfile "/ix/djishnu/Javad/racer/racer-genetics/racer-imputed/racer-mega-imputed-f-matched-v3-QC"    --calc-kins-direct k0.25   --power -.25
+
+
