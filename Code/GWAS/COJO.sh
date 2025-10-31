696003d97b297c211c16cbd372891205099ae63d
Javad Rahimikollu <jrahimik1986@gmail.com>
2025-10-30 21:09:11 -0400

    Add COJO analysis commands for chromosome 6
    Added commands to run COJO analysis with specified parameters for chromosome 6.

---
+++ Code/GWAS/COJO.sh
@@ -0,0 +1,27 @@
+/ix/djishnu/Javad/racer/racer-genetics/racer-imputed/gcta/gcta-1.94.1-linux-kernel-3-x86_64/gcta-1.94.1 \
+  --bfile racer-mega-imputed-f-matched-v3-QC-chr6 \
+  --chr 6 \
+  --maf 0.001 \
+  --cojo-file chr6_cojo.input \
+  --extract-region-bp 6 32491694 65000   \
+  --cojo-slct \
+  --out chr6_cojo.output
+ 
+/ix/djishnu/Javad/racer/racer-genetics/racer-imputed/gcta/gcta-1.94.1-linux-kernel-3-x86_64/gcta-1.94.1 \
+  --bfile racer-mega-imputed-f-matched-v3-QC-chr6 \
+  --chr 6 \
+  --maf 0.001 \
+  --cojo-file chr6_cojo.input \
+  --cojo-cond cond.snplist\
+  --out chr6_con.res
+
+
+
+/ix/djishnu/Javad/racer/racer-genetics/racer-imputed/gcta/gcta-1.94.1-linux-kernel-3-x86_64/gcta-1.94.1 \
+  --bfile racer-mega-imputed-f-matched-v3-QC-chr6 \
+  --chr 6 \
+  --maf 0.001 \
+  --cojo-file chr6_cojo.input \
+  --cojo-cond cond.snplist2\
+  --out chr6_con.res2
+
